7534523aebe70ebcd476bf976738fa48e475a474
Renzo Minelli <renminelli@gmail.com>
2018-08-03 19:52:20 -0300

    cosas

--- BestoGameEver/Form1.Designer.vb
+++ BestoGameEver/Form1.Designer.vb
@@ -23,7 +23,6 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.CorrerAtras = New System.Windows.Forms.Timer(Me.components)
         Me.CorrerDelante = New System.Windows.Forms.Timer(Me.components)
         Me.Movimiento = New System.Windows.Forms.Timer(Me.components)
@@ -32,17 +31,14 @@ Partial Class Form1
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.Panel2 = New System.Windows.Forms.Panel()
         Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Idle = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.SaltoAnima = New System.Windows.Forms.Timer(Me.components)
+        Me.BajoAnima = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel4 = New System.Windows.Forms.Panel()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.Image = Global.ya_ni_se.My.Resources.Resources._19
-        Me.PictureBox1.Location = New System.Drawing.Point(344, 200)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(93, 147)
-        Me.PictureBox1.TabIndex = 0
-        Me.PictureBox1.TabStop = False
         '
         'CorrerAtras
         '
@@ -87,18 +83,61 @@ Partial Class Form1
         Me.Panel3.Size = New System.Drawing.Size(47, 355)
         Me.Panel3.TabIndex = 3
         '
+        'Idle
+        '
+        Me.Idle.Interval = 200
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.ya_ni_se.My.Resources.Resources._01
+        Me.PictureBox1.Location = New System.Drawing.Point(382, 302)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(47, 54)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'SaltoAnima
+        '
+        Me.SaltoAnima.Interval = 1
+        '
+        'BajoAnima
+        '
+        Me.BajoAnima.Interval = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(80, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 25)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Label1"
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.SaddleBrown
+        Me.Panel4.Location = New System.Drawing.Point(173, 236)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(429, 38)
+        Me.Panel4.TabIndex = 5
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(827, 417)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Panel3)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
-        Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Form1"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
@@ -110,5 +149,10 @@ Partial Class Form1
     Friend WithEvents Panel1 As System.Windows.Forms.Panel
     Friend WithEvents Panel2 As System.Windows.Forms.Panel
     Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents Idle As System.Windows.Forms.Timer
+    Friend WithEvents SaltoAnima As System.Windows.Forms.Timer
+    Friend WithEvents BajoAnima As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Panel4 As System.Windows.Forms.Panel
 
 End Class
--- BestoGameEver/Form1.vb
+++ BestoGameEver/Form1.vb
@@ -1,11 +1,11 @@
 ﻿Public Class Form1
 
     '////////////////////////////////////PERSONALIZACION///////////////////////
-    Public avanzar As Double = 1
-    Public acelereacion As Double = 0.1
+    Public avanzar As Double = 5
+    Public acelereacion As Double = 0
     Public limvel As Double = 5
     Public salto As Double = 20
-    Public velsubida As Double = 5
+    Public velsubida As Double = 8
     Public desasubida As Double = 0.01
     Public caida As Double = 5
     Public acelcaida As Double = 0.01
@@ -20,28 +20,37 @@
     Dim caida2 As Double = caida
     Dim acelcaida2 As Double = acelcaida
 
-
+    Dim bajo As Integer = 0
+    Dim sal As Integer = 0
+    Dim lado As Integer = 0
+    Dim stand As Integer = 0
     Dim foto As Integer = 7
     Dim d As Integer
     Dim a As Integer
     Dim cont As Integer = 0
 
+
     Private Sub Form1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
         If e.KeyCode = Keys.A Then
+
+
             a = 1
             Movimiento.Enabled = True
-
+            lado = 0
         End If
         If e.KeyCode = Keys.D Then
+            
 
             d = 1
+            lado = 1
             Movimiento.Enabled = True
 
         End If
-        If e.KeyCode = Keys.W Then
+        If e.KeyCode = Keys.W Or e.KeyCode = Keys.Space Then
 
             If Descenso.Enabled = False Then
 
+                SaltoAnima.Enabled = True
                 Ascenso.Enabled = True
 
             End If
@@ -53,18 +62,27 @@
 
     Private Sub Form1_KeyUp(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyUp
         If e.KeyCode = Keys.A Then
-            PictureBox1.Image = My.Resources._19
+
             Movimiento.Enabled = False
             CorrerAtras.Enabled = False
+
+            lado = 0
+            Idle.Enabled = True
+
             avanzar2 = avanzar
             a = 0
         End If
         If e.KeyCode = Keys.D Then
-            PictureBox1.Image = My.Resources._19
+
             Movimiento.Enabled = False
             CorrerDelante.Enabled = False
+
+            lado = 1
+            Idle.Enabled = True
+
             avanzar2 = avanzar
             d = 0
+
         End If
         If e.KeyCode = Keys.W Then
 
@@ -92,7 +110,7 @@
                     avanzar2 += acelereacion2
                 End If
 
-
+                Idle.Enabled = False
                 CorrerDelante.Enabled = True
 
 
@@ -108,7 +126,7 @@
                     avanzar2 += acelereacion2
                 End If
 
-
+                Idle.Enabled = False
                 CorrerAtras.Enabled = True
 
                 If PictureBox1.Location.X <= -10 Then
@@ -120,37 +138,82 @@
                 CorrerDelante.Enabled = False
                 CorrerAtras.Enabled = False
                 Movimiento.Enabled = False
-                PictureBox1.Image = My.Resources._1
+                Idle.Enabled = True
             End If
 
         End If
+        If Descenso.Enabled = False Then
+            If PictureBox1.Location.X < Panel4.Location.X - 20 Or PictureBox1.Location.X > Panel4.Location.X + Panel4.Width Then
 
+                Descenso.Enabled = True
+
+            End If
+        End If
 
         
     End Sub
 
     Private Sub Timer3_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Descenso.Tick
+        SaltoAnima.Enabled = False
 
-        Dim altura As Double = PictureBox1.Location.Y
+        'Guardo las posiciones del picturebox y del panel(plataforma)
+        Dim y As Double = PictureBox1.Location.Y
+        Dim x As Double = PictureBox1.Location.X
+        Dim py As Double = Panel4.Location.Y
+        Dim px As Double = Panel4.Location.X
 
-        If (altura + caida2) <= 204 Then
+        Label1.Text = "PanelY: " + py.ToString + vbNewLine + "Panelx: " + px.ToString + " hasta: " + (px + 325).ToString + vbNewLine + "Y: " + y.ToString + vbNewLine + "X: " + x.ToString
 
-            PictureBox1.Location = New Point(PictureBox1.Location.X, altura + caida2)
+        If y >= py - 30 And y <= py And x > px And x < px + 430 Then
 
-            caida2 += acelcaida2
 
-        ElseIf altura <= 204 Then
+            PictureBox1.Location = New Point(x, py - 100)
+            Label1.Text += vbNewLine + "Detected"
 
-            PictureBox1.Location = New Point(PictureBox1.Location.X, 200)
+
 
             caida2 = caida
             acelcaida2 = acelcaida
+
+
+
+            BajoAnima.Enabled = False
+            Idle.Enabled = True
             Descenso.Enabled = False
+
+        End If
+
+        If (y + caida2) <= 302 Then
+
+            PictureBox1.Location = New Point(PictureBox1.Location.X, y + caida2)
+
+            caida2 += acelcaida2
+
+        ElseIf y <= 302 Then
+
+            PictureBox1.Location = New Point(PictureBox1.Location.X, 302)
+
+            caida2 = caida
+            acelcaida2 = acelcaida
+
+            BajoAnima.Enabled = False
+
+
+            Idle.Enabled = True
+
+            Descenso.Enabled = False
+
 
         Else
 
             caida2 = caida
             acelcaida2 = acelcaida
+
+            BajoAnima.Enabled = False
+
+
+            Idle.Enabled = True
+
             Descenso.Enabled = False
 
         End If
@@ -166,39 +229,25 @@
         Select Case foto
 
             Case 7
-                PictureBox1.Image = My.Resources._8
+                PictureBox1.Image = My.Resources._13
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
                 foto += 1
             Case 8
-                PictureBox1.Image = My.Resources._9
+                PictureBox1.Image = My.Resources._14
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
                 foto += 1
             Case 9
-                PictureBox1.Image = My.Resources._10
+                PictureBox1.Image = My.Resources._15
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
                 foto += 1
             Case 10
-                PictureBox1.Image = My.Resources._11
+                PictureBox1.Image = My.Resources._16
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
                 foto += 1
             Case 11
-                PictureBox1.Image = My.Resources._12
-                foto += 1
-            Case 12
-                PictureBox1.Image = My.Resources._13
-                foto += 1
-            Case 13
-                PictureBox1.Image = My.Resources._14
-                foto += 1
-            Case 14
-                PictureBox1.Image = My.Resources._15
-                foto += 1
-            Case 15
-                PictureBox1.Image = My.Resources._16
-                foto += 1
-            Case 16
                 PictureBox1.Image = My.Resources._17
-                foto += 1
-            Case 17
-                PictureBox1.Image = My.Resources._18
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
                 foto = 7
-
 
 
         End Select
@@ -206,7 +255,7 @@
 
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
+        Idle.Enabled = True
         inicio.Show()
 
     End Sub
@@ -218,19 +267,22 @@
         If cont <= salto2 Then
 
             PictureBox1.Location = New Point(PictureBox1.Location.X, PictureBox1.Location.Y - velsubida2)
-            'Label1.Text = velsubida2.ToString
+
             cont += 1
             velsubida2 -= desasubida2
+
 
         Else
             velsubida2 = velsubida
             Ascenso.Enabled = False
             Descenso.Enabled = True
+            BajoAnima.Enabled = True
             cont = 0
+
         End If
 
 
-
+       
 
 
 
@@ -238,57 +290,160 @@
 
 
     Private Sub correr_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CorrerAtras.Tick
-        Dim imagen As Image
+
 
         Select Case foto
 
-            Case 7
-                PictureBox1.Image = My.Resources._8
-                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+             Case 7
+                PictureBox1.Image = My.Resources._13
                 foto += 1
             Case 8
-                PictureBox1.Image = My.Resources._9
-                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+                PictureBox1.Image = My.Resources._14
                 foto += 1
             Case 9
-                PictureBox1.Image = My.Resources._10
-                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+                PictureBox1.Image = My.Resources._15
                 foto += 1
             Case 10
-                PictureBox1.Image = My.Resources._11
-                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+                PictureBox1.Image = My.Resources._16
                 foto += 1
             Case 11
-                PictureBox1.Image = My.Resources._12
-                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
-                foto += 1
-            Case 12
-                PictureBox1.Image = My.Resources._13
-                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
-                foto += 1
-            Case 13
-                PictureBox1.Image = My.Resources._14
-                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
-                foto += 1
-            Case 14
-                PictureBox1.Image = My.Resources._15
-                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
-                foto += 1
-            Case 15
-                PictureBox1.Image = My.Resources._16
-                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
-                foto += 1
-            Case 16
                 PictureBox1.Image = My.Resources._17
-                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
-                foto += 1
-            Case 17
-                PictureBox1.Image = My.Resources._18
-                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
                 foto = 7
 
-
-
         End Select
+
+    End Sub
+
+    Private Sub Idle_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Idle.Tick
+        CorrerDelante.Enabled = False
+        CorrerAtras.Enabled = False
+
+        If lado = 0 Then
+            Select Case stand
+                Case 0
+                    PictureBox1.Image = My.Resources._0
+                    stand += 1
+                Case 1
+                    PictureBox1.Image = My.Resources._1
+                    stand += 1
+                Case 2
+                    PictureBox1.Image = My.Resources._2
+                    stand += 1
+                Case 3
+                    PictureBox1.Image = My.Resources._3
+                    stand += 1
+                Case 4
+                    PictureBox1.Image = My.Resources._4
+                    stand = 0
+            End Select
+
+        ElseIf lado = 1 Then
+
+            Select Case stand
+                Case 0
+                    PictureBox1.Image = My.Resources._0
+                    PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+                    stand += 1
+                Case 1
+                    PictureBox1.Image = My.Resources._1
+                    PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+                    stand += 1
+                Case 2
+                    PictureBox1.Image = My.Resources._2
+                    PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+                    stand += 1
+                Case 3
+                    PictureBox1.Image = My.Resources._3
+                    PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+                    stand += 1
+                Case 4
+                    PictureBox1.Image = My.Resources._4
+                    PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+                    stand = 0
+            End Select
+        End If
+       
+    End Sub
+
+    Private Sub SaltoAnima_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SaltoAnima.Tick
+       
+
+        Idle.Enabled = False
+
+        If lado = 0 Then
+
+            If sal >= 0 And sal < 5 Then
+                PictureBox1.Image = My.Resources._01
+                sal += 1
+            ElseIf sal >= 5 And sal < 10 Then
+                PictureBox1.Image = My.Resources._20
+                sal += 1
+            Else
+                PictureBox1.Image = My.Resources.a
+            End If
+
+        ElseIf lado = 1 Then
+
+            If sal >= 0 And sal < 5 Then
+                PictureBox1.Image = My.Resources._01
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+                sal += 1
+            ElseIf sal >= 5 And sal < 10 Then
+                PictureBox1.Image = My.Resources._20
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+                sal += 1
+            Else
+                PictureBox1.Image = My.Resources.a
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+            End If
+
+        End If
+
+       
+
+
+    End Sub
+
+    Private Sub BajoAnima_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BajoAnima.Tick
+
+        SaltoAnima.Enabled = False
+
+        If lado = 0 Then
+
+            If bajo >= 0 And bajo < 5 Then
+                PictureBox1.Image = My.Resources._18
+                bajo += 1
+            ElseIf bajo >= 5 And bajo < 10 Then
+                PictureBox1.Image = My.Resources.b
+                bajo += 1
+            Else
+                PictureBox1.Image = My.Resources.c
+
+            End If
+
+        ElseIf lado = 1 Then
+
+            If bajo >= 0 And bajo < 5 Then
+                PictureBox1.Image = My.Resources._18
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+                bajo += 1
+            ElseIf bajo >= 5 And bajo < 10 Then
+                PictureBox1.Image = My.Resources.b
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+                bajo += 1
+            Else
+                PictureBox1.Image = My.Resources.c
+                PictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+
+            End If
+
+        End If
+        
+
+
+    End Sub
+
+    Private Sub Esfera_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
     End Sub
 End Class
